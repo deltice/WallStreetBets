6cd28fedd23d53ac283ec64158062d7223817d6b
deltice <frankli@princeton.edu>
2017-11-12 05:57:31 -0500

    add example website

---
+++ website_example/MemeStock.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  var stocks = [
+    ["BABA", 186.41, 3268, "Long"],
+    ["MTCH", 28.46, 1434, "Short"],
+    ["NFLX", 192.02, 15, "Short"],
+    ["SNAP", 12.76, 1599, "Short"],
+    ["ALGN", 249.46, 210, "Short"],
+    ["JD", 39.96, 6566, "Long"],
+    ["FIZZ", 96.41, 120, "Short"]
+  ];
+
+  stocks.forEach(function(stock) {
+    newStock = `<div id=${stock[0]}><h3>$${stock[0]}</h3><ul><li>Shares: ${
+      stock[2]
+    }</li><li>Equity: $${(stock[2] * stock[1]).toLocaleString(undefined, {
+      minimumFractionDigits: 2
+    })}</li><li>Position: ${stock[3]}</li></ul><br>`;
+    $("#stockInfo").append(newStock);
+  });
+});
